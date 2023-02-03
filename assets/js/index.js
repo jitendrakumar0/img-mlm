653ef500ed74d3da1e4588b612bf70a058021a27
sureshkumawatimg <101689063+sureshkumawatimg@users.noreply.github.com>
2023-02-03 17:20:28 +0530

    worked on website
    Create company pages,
    create gallery page

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,125 +16,208 @@ $(window).scroll(function () {
     }
 });
 
-
-// Home page hero slider
-  var owl = $('.heorSection');
+//? ###################################### Home Page JS ######################################
+if ($("[page-name=homePage]").length) {
+    // Home page hero slider
+      var owl = $('.heorSection');
+        owl.owlCarousel({
+            margin: 10,
+            loop: true,
+            dots:true,
+            autoplay:true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:false,
+            responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+            }
+        })
+    
+    // Main Event slider
+    var owl = $('.mainEventSlider');
     owl.owlCarousel({
-        margin: 10,
+        margin: 20,
         loop: true,
         dots:true,
+        nav:true,
         autoplay:true,
         autoplayTimeout:3000,
         autoplayHoverPause:false,
+        navText: ["<img class='w-100' src='assets/img/icons/leftArrow.svg'>","<img class='w-100' src='assets/img/icons/rightArrow.svg''>"],
         responsive: {
         0: {
-            items: 1
+            items: 1,
+            nav:false,
+            stagePadding: 30,
+            margin: 15,
         },
-        600: {
-            items: 1
+        576: {
+            items: 1,
+            nav:false,
+            stagePadding: 100,
         },
-        1000: {
-            items: 1
+        992: {
+            items: 3
         }
         }
     })
-
-// Main Event slider
-var owl = $('.mainEventSlider');
-owl.owlCarousel({
-    margin: 20,
-    loop: true,
-    dots:true,
-    nav:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:false,
-    navText: ["<img class='w-100' src='assets/img/icons/leftArrow.svg'>","<img class='w-100' src='assets/img/icons/rightArrow.svg''>"],
-    responsive: {
-    0: {
-        items: 1,
-        nav:false,
-        stagePadding: 30,
-        margin: 15,
-    },
-    576: {
-        items: 1,
-        nav:false,
-        stagePadding: 100,
-    },
-    992: {
-        items: 3
-    }
-    }
-})
-
-// GLightbox js 
-var lightbox = GLightbox();
-lightbox.on('open', (target) => {
-    console.log('lightbox opened');
-});
-var lightboxDescription = GLightbox({
-    selector: '.glightbox2'
-});
-var lightboxVideo = GLightbox({
-    selector: '.glightbox3'
-});
-lightboxVideo.on('slide_changed', ({ prev, current }) => {
-    console.log('Prev slide', prev);
-    console.log('Current slide', current);
-
-    const { slideIndex, slideNode, slideConfig, player } = current;
-
-    if (player) {
-        if (!player.ready) {
-            // If player is not ready
-            player.on('ready', (event) => {
-                // Do something when video is ready
+    
+    // GLightbox js 
+    var lightbox = GLightbox();
+    lightbox.on('open', (target) => {
+        console.log('lightbox opened');
+    });
+    var lightboxDescription = GLightbox({
+        selector: '.glightbox2'
+    });
+    var lightboxVideo = GLightbox({
+        selector: '.glightbox3'
+    });
+    lightboxVideo.on('slide_changed', ({ prev, current }) => {
+        console.log('Prev slide', prev);
+        console.log('Current slide', current);
+    
+        const { slideIndex, slideNode, slideConfig, player } = current;
+    
+        if (player) {
+            if (!player.ready) {
+                // If player is not ready
+                player.on('ready', (event) => {
+                    // Do something when video is ready
+                });
+            }
+    
+            player.on('play', (event) => {
+                console.log('Started play');
+            });
+    
+            player.on('volumechange', (event) => {
+                console.log('Volume change');
+            });
+    
+            player.on('ended', (event) => {
+                console.log('Video ended');
             });
         }
-
-        player.on('play', (event) => {
-            console.log('Started play');
-        });
-
-        player.on('volumechange', (event) => {
-            console.log('Volume change');
-        });
-
-        player.on('ended', (event) => {
-            console.log('Video ended');
-        });
-    }
-});
-
-var lightboxInlineIframe = GLightbox({
-    selector: '.glightbox4'
-});
-
-
-// bootstrap form validation   
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-
-//   top select 
-    new TomSelect(".tomSelect",{
+    });
+    
+    var lightboxInlineIframe = GLightbox({
+        selector: '.glightbox4'
+    });
+    
+    // form validation
+    (() => {
+        'use strict'
+      
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+      
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+          form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+      
+            form.classList.add('was-validated')
+          }, false)
+        })
+      })()
+    
+    
+    //   top select 
+    new TomSelect(".tomSelect", {
         allowEmptyOption: false,
         create: false
     });
+}
+//! ###################################### Home Page JS ######################################
+
+//? ###################################### Company Page JS ######################################
+if ($("[page-name=companyPage]").length) {
+    
+    // Read More & Read Less 
+    let redeMoreBtn = document.querySelectorAll('.redeMoreBtn');
+    redeMoreBtn.forEach(redeMoreBtns =>{
+        redeMoreBtns.addEventListener('click', (e)=>{
+            let a = e.currentTarget;
+            a.classList.toggle('newTxt');
+            a.previousElementSibling.classList.toggle('showText');
+            if($('.redeMoreBtn').hasClass('newTxt')) {
+                a.innerHTML = 'Read Less';
+                a.style = "color: red !important;";
+            }else{
+                a.innerHTML = 'Read More';
+                a.style = "color: $theme2 !important;";
+            };
+        });
+    });
+}
+
+//! ###################################### Company Page JS ######################################
+
+//? ###################################### Gallery Page JS ######################################
+
+if ($("[page-name=galleryPage]").length) {
+    
+    // GLightbox js 
+    var lightbox = GLightbox();
+    lightbox.on('open', (target) => {
+        console.log('lightbox opened');
+    });
+    var lightboxDescription = GLightbox({
+        selector: '.glightbox2'
+    });
+    var lightboxVideo = GLightbox({
+        selector: '.glightbox3'
+    });
+    lightboxVideo.on('slide_changed', ({ prev, current }) => {
+        console.log('Prev slide', prev);
+        console.log('Current slide', current);
+    
+        const { slideIndex, slideNode, slideConfig, player } = current;
+    
+        if (player) {
+            if (!player.ready) {
+                // If player is not ready
+                player.on('ready', (event) => {
+                    // Do something when video is ready
+                });
+            }
+    
+            player.on('play', (event) => {
+                console.log('Started play');
+            });
+    
+            player.on('volumechange', (event) => {
+                console.log('Volume change');
+            });
+    
+            player.on('ended', (event) => {
+                console.log('Video ended');
+            });
+        }
+    });
+    
+    var lightboxInlineIframe = GLightbox({
+        selector: '.glightbox4'
+    });
+
+    // Justify Gallery
+    $("#myGalleryAlbum").justifiedGallery({
+        rowHeight: 180,
+        lastRow: 'nojustify',
+        margins: 16,
+        captions: false
+    });
+}
+
+//! ###################################### Gallery Page JS ######################################

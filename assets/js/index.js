5e316116cae8225f3dcce913b3ecbdeccbff2356
sureshkumawatimg <101689063+sureshkumawatimg@users.noreply.github.com>
2023-02-07 17:17:43 +0530

    create news page
    Changes in home page,
    create news page

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,21 @@
 $(function(){
     $("#myHeader").load("layout/header.html"); 
     $("#myFooter").load("layout/footer.html"); 
+    $(".myPreloader").load("layout/preloader.html"); 
   });
 
-  
+//   var preloaderMain = document.querySelector(".myPreloader");
+//     window.addEventListener('load', function(){
+//         setTimeout(()=>{
+//             preloaderMain.classList.add("d-none");
+//             document.body.style = "overflow: hidden auto !important;"
+//           }, 1000);
+//     })
+
+// setTimeout(()=>{
+//     document.querySelector(".myPreloader").style = 'visibility: hidden; transition: ease-in-out .3s;'
+//   }, 1000);
+
 // header sticky   
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
@@ -212,7 +224,7 @@ if ($("[page-name=galleryPage]").length) {
     });
 
     // Justify Gallery
-    $("#myGalleryAlbum").justifiedGallery({
+    $(".myJustifyGallery").justifiedGallery({
         rowHeight: 180,
         lastRow: 'nojustify',
         margins: 16,
@@ -221,3 +233,130 @@ if ($("[page-name=galleryPage]").length) {
 }
 
 //! ###################################### Gallery Page JS ######################################
+
+
+
+//? ###################################### News Page JS ######################################
+if ($("[page-name=newsPage]").length) {
+    // Main Event slider
+    var owl = $('.offerSlider');
+    owl.owlCarousel({
+        margin: 20,
+        loop: true,
+        dots:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        navText: ["<img class='w-100' src='assets/img/icons/leftArrow.svg'>","<img class='w-100' src='assets/img/icons/rightArrow.svg''>"],
+        responsive: {
+        0: {
+            items: 1,
+            nav:false,
+            stagePadding: 30,
+            margin: 15,
+        },
+        576: {
+            items: 1,
+            nav:false,
+            stagePadding: 100,
+        },
+        992: {
+            items: 3
+        }
+        }
+    })
+
+    // Event slider
+    var owl = $('.eventVideoSlider');
+    owl.owlCarousel({
+        margin: 20,
+        loop: true,
+        dots:true,
+        nav:true,
+        // autoplay:true,
+        // autoplayTimeout:3000,
+        // autoplayHoverPause:false,
+        navText: ["<img class='w-100' src='assets/img/icons/leftArrow.svg'>","<img class='w-100' src='assets/img/icons/rightArrow.svg''>"],
+        responsive: {
+        0: {
+            items: 1,
+            nav:false,
+            stagePadding: 50,
+            margin: 15,
+        },
+        380: {
+            items: 1,
+            nav:false,
+            stagePadding: 60,
+            margin: 15,
+        },
+        576: {
+            items: 2,
+            nav:false,
+            stagePadding: 50,
+        },
+        992: {
+            items: 4
+        }
+        }
+    })
+
+    // bootstrap tooltip 
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // GLightbox js 
+    var lightbox = GLightbox();
+    lightbox.on('open', (target) => {
+        console.log('lightbox opened');
+    });
+    var lightboxDescription = GLightbox({
+        selector: '.glightbox2'
+    });
+    var lightboxVideo = GLightbox({
+        selector: '.glightbox3'
+    });
+    lightboxVideo.on('slide_changed', ({ prev, current }) => {
+        console.log('Prev slide', prev);
+        console.log('Current slide', current);
+    
+        const { slideIndex, slideNode, slideConfig, player } = current;
+    
+        if (player) {
+            if (!player.ready) {
+                // If player is not ready
+                player.on('ready', (event) => {
+                    // Do something when video is ready
+                });
+            }
+    
+            player.on('play', (event) => {
+                console.log('Started play');
+            });
+    
+            player.on('volumechange', (event) => {
+                console.log('Volume change');
+            });
+    
+            player.on('ended', (event) => {
+                console.log('Video ended');
+            });
+        }
+    });
+    
+    var lightboxInlineIframe = GLightbox({
+        selector: '.glightbox4'
+    });
+
+    // Justify Gallery
+    $(".myJustifyGallery").justifiedGallery({
+        rowHeight: 180,
+        lastRow: 'nojustify',
+        margins: 16,
+        captions: false
+    });
+
+    
+}
+//! ###################################### News Page JS ######################################
